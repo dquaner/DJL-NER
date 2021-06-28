9eeedf715c2e9799ef458e220bb2ea3f2e5181be
d_quaner <849358686>
2021-06-29 03:31:06 +0800

    add evaluate

--- demo/src/main/java/ner/TokensTest.java
+++ demo/src/main/java/ner/TokensTest.java
@@ -10,27 +10,46 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TokensTest {
     public static void main(String[] args) throws Exception {
-        Predictor<String[], String[]> predictor = getPredictor("tiny-dbmdz");
+        Predictor<String[], String[]> predictor = getPredictor("elastic");
         List<String[][]> data = getTestData();
         System.out.println(data.size());
+        List<String> predict = new ArrayList<>();
+        List<String> label = new ArrayList<>();
         int count = 1;
         for (String[][] sequence: data) {
-            if (count > 10) {
-                break;
-            }
             System.out.println("------------ sequence(" +  count + ") ------------");
+            label.addAll(Arrays.asList(sequence[1]));
             String[] result = predictor.predict(sequence[0]);
-            for (int i = 0; i < result.length; i++) {
-                System.out.println(sequence[1][i] + " -> " + result[i]);
-            }
+            predict.addAll(Arrays.asList(result));
             count ++;
         }
+        System.out.println(predict.size());
+        System.out.println(label.size());
+        String[] entities = new String[]{"LOC", "ORG", "PER", "MISC"};
+        double[] total_metrics = new double[]{0.0, 0.0, 0.0, 0.0, 0.0};
+        for (String entity: entities) {
+            double[] metrics = evaluate(entity, predict.toArray(new String[0]), label.toArray(new String[0]));
+            for (int i=0; i<total_metrics.length; i++) {
+                total_metrics[i] += metrics[i];
+            }
+            System.out.println(entity + ": " + metrics[3] + ", " + metrics[4] + ", " + metrics[5]);
+        }
+        total_metrics[3] /= entities.length;
+        total_metrics[4] /= entities.length;
+        double micro_precision = (total_metrics[0] + total_metrics[1]) == 0 ?
+                Double.POSITIVE_INFINITY : total_metrics[0] / (total_metrics[0] + total_metrics[1]);
+        double micro_recall = (total_metrics[0] + total_metrics[2]) == 0 ?
+                Double.POSITIVE_INFINITY : total_metrics[0] / (total_metrics[0] + total_metrics[2]);
+        double micro_f1 = (micro_precision == Double.POSITIVE_INFINITY || micro_recall == Double.POSITIVE_INFINITY) ?
+                Double.POSITIVE_INFINITY : 2 * micro_precision * micro_recall / (micro_precision + micro_recall);
+        System.out.println("micro_f1: " + micro_f1);
 
-
+        System.out.println("macro_f1: " + 2 * total_metrics[3] * total_metrics[4] / (total_metrics[3] + total_metrics[4]));
     }
 
     public static List<String[][]> getTestData() throws IOException {
@@ -65,4 +84,30 @@ public class TokensTest {
                 .build();
         return ModelZoo.loadModel(criteria).newPredictor(translator);
     }
+
+    public static double[] evaluate(String entity, String[] predict, String[] label) {
+        double[] metrics = new double[6];
+        int tp = 0, fp = 0, fn = 0;
+        for (int i = 0; i < predict.length; i++) {
+            if (predict[i].endsWith(entity) && label[i].endsWith(entity)) {
+                tp++;
+            } else if (predict[i].endsWith(entity) && !label[i].endsWith(entity)) {
+                fp++;
+            } else if (!predict[i].endsWith(entity) && label[i].endsWith(entity)) {
+                fn++;
+            }
+        }
+        metrics[0] = tp;
+        metrics[1] = fp;
+        metrics[2] = fn;
+        metrics[3] = (tp + fp) == 0 ? Double.POSITIVE_INFINITY : tp / (double) (tp + fp);
+        metrics[4] = (tp + fn) == 0 ? Double.POSITIVE_INFINITY : tp / (double) (tp + fn);
+        if (metrics[3] == Double.POSITIVE_INFINITY || metrics[4] == Double.POSITIVE_INFINITY) {
+            metrics[5] = Double.POSITIVE_INFINITY;
+        } else {
+            metrics[5] = 2 * metrics[3] * metrics[4] / (metrics[3] + metrics[4]);
+        }
+        return metrics;
+    }
+
 }
